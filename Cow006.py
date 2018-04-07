0442ef8d53c438f53a2c25a481645ae2cece7f97
Caezer27 <zhirkovich@mail.ru>
2018-04-07 19:04:59 +0300

    added constants, modifed code (less big functions)

--- Cow006.py
+++ Cow006.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 
-# In[1]:
-
 from random import shuffle, choice
 import copy
 
+
 class Card006():
-    
-    CARD_SCORE = {x: 1 for x in range(1, 105)}
+
+    MAX_CARD_NUMBER = 104
+    CARD_SCORE = {x: 1 for x in range(1, MAX_CARD_NUMBER+1)}
+
     @staticmethod
     def all_cards(random = False):
         if random:
@@ -19,7 +20,7 @@ class Card006():
     
     def __init__(self, number):
         def _det_score(number1):
-            if number1 == 55:
+            if number1 % 55 == 0:
                 return 7
             elif number1 % 11 == 0:
                 return 5
@@ -28,13 +29,13 @@ class Card006():
             elif number1 % 5 == 0:
                 return 2
             else:
-                return 1    
+                return 1
         self._validate(number)
         self.card_number = number    
         self.card_score = _det_score(number)
 
     def _validate(self, number):
-        if not (number > 0 and number < 105):
+        if not (number > 0 and number < Card006.MAX_CARD_NUMBER+1):
             raise ValueError('Invalid card\'s number')
     
     def __eq__(self, other):                                      #Проверять ли что other - card006???
@@ -66,15 +67,19 @@ class Card006():
 
 
 class Row():
-    
+
+    MAX_LEN_OF_ROW = 6
+
     def __init__(self, card, row_id):
-        self._validate(card)
+        self._validate(card, row_id)
         self.list_card = [card]
         self.row_id = row_id
 
-    def _validate(self, card):
+    def _validate(self, card, row_id):                        #Что значит _функция???
         if not (isinstance(card, Card006)):
             raise ValueError('Invalid row: must be initalised with Card006')
+        if not (isinstance(row_id, int)):
+            raise ValueError('Invalid row: row_id must be integer')
             
     def __str__(self):
         return 'Row №{}: {}'.format(self.row_id+1, ' '.join(str(card) for card in self.list_card))
@@ -86,52 +91,53 @@ class Row():
         self.list_card.append(card)
 
     def accept_card(self, card):
-        self._validate(card)
+        self._validate(card, 1)
         if self.list_card[-1] < card:
             return True
         else:
             return False
 
     def check6(self):
-        if len(self.list_card) == 5:
+        if len(self.list_card) == Row.MAX_LEN_OF_ROW-1:
             return True
         else:
             return False
 
 
 class Table():
+
+    NUMBER_OF_ROWS = 4
     
     def __init__(self, cards):
         if not all(isinstance(card, Card006) for card in cards):
             raise ValueError(
                 'Invalid table: all cards must be Card006 objects'
             )
-        if len(cards) != 4:
+        if len(cards) != Table.NUMBER_OF_ROWS:
             raise ValueError(
-                'Invalid table: must be initalised with 4 Cards006'
+                'Invalid table: must be initalised with {} Cards006'.format(Table.NUMBER_OF_ROWS)
             )
-        self.rows = [Row(cards[i], i) for i in range(4)]
+        self.rows = [Row(cards[i], i) for i in range(Table.NUMBER_OF_ROWS)]
         
     def __repr__(self):
         cards_of_rows = list(map(str, self.rows))
-        return '<CowTable object. Rows:\n{c[0]}\n{c[1]}\n{c[2]}\n{c[3]}>'.format(c = cards_of_rows)
+        return '<CowTable object. Rows:\n{}>'.format('\n'.join(cards for cards in cards_of_rows))
     
     def __str__(self):
         cards_of_rows = list(map(str, self.rows))
-        return 'Current table:\n{c[0]}\n{c[1]}\n{c[2]}\n{c[3]}'.format(c = cards_of_rows)
+        return 'Current table:\n{}'.format('\n'.join(cards for cards in cards_of_rows))
 
 
 class CowPlayer():
-    
+
+    INITIAL_NUMBER_OF_CARDS = 10
+
     def __init__(self, cards, player_id=None, human=False):
-        if len(cards) != 10:
+        if len(cards) != CowPlayer.INITIAL_NUMBER_OF_CARDS:
             raise ValueError(
-                'Invalid player: must be initalised with 10 Cards006'
-            )
+                'Invalid player: must be initalised with {} Cards006'.format(CowPlayer.INITIAL_NUMBER_OF_CARDS))
         if not all(isinstance(card, Card006) for card in cards):
-            raise ValueError(
-                'Invalid player: cards must all be Card006 objects'
-            )
+            raise ValueError('Invalid player: cards must all be Card006 objects')
         self.hand = cards
         self.player_id = player_id
         self.penalty_score = 0
@@ -164,13 +170,17 @@ class CowPlayer():
     
 class CowGame():
 
+    MIN_NUMBER_OF_PLAYERS = 2
+    TOTAL_NUMBER_OF_PLAYERS = 10
     CRITICIAL_PENALTY_SCORE = 66
 
     def __init__(self, human_players, ii_players):
         if not (isinstance(human_players, int) and isinstance(ii_players, int)):
             raise ValueError('Invalid game: number of players must be integer')
-        if not 2 <= human_players + ii_players <= 10:
-            raise ValueError('Invalid game: total number of players must be between 2 and 10 players')
+        if not CowGame.MIN_NUMBER_OF_PLAYERS <= human_players + ii_players <= CowGame.TOTAL_NUMBER_OF_PLAYERS:
+            raise ValueError(
+                'Invalid game: total number of players must be between {} and {} players'
+                .format(CowGame.MIN_NUMBER_OF_PLAYERS, CowGame.TOTAL_NUMBER_OF_PLAYERS))
         self.clear(True, human_players, ii_players)
         self.playable_cards = []
 
@@ -182,10 +192,10 @@ class CowGame():
         else:
             for player in self.players:
                 player.hand = self.deal_hand()
-        self.table = Table([self.deck.pop() for i in range(4)])
+        self.table = Table([self.deck.pop() for i in range(Table.NUMBER_OF_ROWS)])
     
     def deal_hand(self):
-        return [self.deck.pop() for i in range(10)]
+        return [self.deck.pop() for i in range(CowPlayer.INITIAL_NUMBER_OF_CARDS)]
     
     @property
     def is_active(self):
@@ -194,50 +204,55 @@ class CowGame():
     def pop_card_player(self, player_id, card_index):
         self.playable_cards.append([self.players[player_id].hand.pop(card_index), player_id])
 
+    def take_row(self, player_id, row_id, card):
+        self.players[player_id].add_cards_to_base(self.table.rows[row_id].list_card)
+        self.table.rows[row_id].list_card.clear()
+        self.table.rows[row_id].add(card)
+        self.print_take_row(player_id, row_id, card)
+
     def human_put_card(self, player_id, card):
         list_comparing = [rows.row_id for rows in self.table.rows if rows.accept_card(card)]
         if len(list_comparing) == 0:
             print(self.table)
             choosed = False
             while not choosed:
-                row_index = int(input('Please select row. ')) - 1
-                if row_index > 3 or row_index < 0:
+                row_id = int(input('Please select row. ')) - 1
+                if row_id > Table.NUMBER_OF_ROWS-1 or row_id < 0:
                     print('Please select an existing row! ')
                 else:
-                    self.players[player_id].add_cards_to_base(self.table.rows[row_index].list_card)
-                    self.table.rows[row_index].list_card.clear()
-                    self.table.rows[row_index].add(card)
+                    self.take_row(player_id, row_id, card)
                     choosed = True
         else:
-            row_id = min([(card - self.table.rows[row].list_card[-1],row) for row in list_comparing])[1]
-            if self.table.rows[row_id].check6():
-                self.players[player_id].add_cards_to_base(self.table.rows[row_id].list_card)
-                self.table.rows[row_id].list_card.clear()
-                self.table.rows[row_id].add(card)
-            else:
-                self.table.rows[row_id].add(card)
+            self.put_card_to_row(list_comparing, player_id, card)
 
     def put_card(self, player_id, card):
         list_comparing = [rows.row_id for rows in self.table.rows if rows.accept_card(card)]
         if len(list_comparing) == 0:
-            row_index = choice(list(range(4)))
-            self.players[player_id].add_cards_to_base(self.table.rows[row_index].list_card)
-            self.table.rows[row_index].list_card.clear()
-            self.table.rows[row_index].add(card)
+            row_id = choice(list(range(Table.NUMBER_OF_ROWS)))
+            self.take_row(player_id, row_id, card)
         else:
-            row_id = min([(card - self.table.rows[row].list_card[-1],row) for row in list_comparing])[1]
-            if self.table.rows[row_id].check6():
-                self.players[player_id].add_cards_to_base(self.table.rows[row_id].list_card)
-                self.table.rows[row_id].list_card.clear()
-                self.table.rows[row_id].add(card)
-            else:
-                self.table.rows[row_id].add(card)
+            self.put_card_to_row(list_comparing, player_id, card)
+
+    def put_card_to_row(self, list_comparing, player_id, card):
+        row_id = min([(card - self.table.rows[row].list_card[-1], row) for row in list_comparing])[1]
+        if self.table.rows[row_id].check6():
+            self.take_row(player_id, row_id, card)
+            self.print_take_row(player_id, row_id, card)
+        else:
+            self.table.rows[row_id].add(card)
+            self.print_put_card(player_id, card, row_id)
+
+    def print_take_row(self, player_id, row_id, card):
+        print('Player№{} take row№{} and put card {}'.format(player_id+1, row_id+1, card))
+
+    def print_put_card(self, player_id, card, row_id):
+        print('Player№{} put card {} to row№{}'.format(player_id+1, card, row_id+1))
 
     def play(self):
         print('The game begins.')
         while self.is_active:
             counter = 1
-            while self.is_active and counter <= 10:
+            while self.is_active and counter <= CowPlayer.INITIAL_NUMBER_OF_CARDS:
                 print()
                 next(self)
                 counter += 1
@@ -256,6 +271,7 @@ class CowGame():
             print('Player №{} has {} penalty scores'.format(player.player_id+1, player.penalty_score))
         print()
 
+        #Выбор карт для игры
         for player in self.players:
             if player.human:
                 player.print_hand()
@@ -273,9 +289,9 @@ class CowGame():
                 card = choice(player.hand)
                 self.pop_card_player(player.player_id, player.hand.index(card))
 
+        #Разложение карт по строкам
         self.playable_cards.sort()
-        print(self.playable_cards)
-
+        #print(self.playable_cards)
         for card_and_player_id in self.playable_cards:
             player_ids = card_and_player_id[1]
             card = card_and_player_id[0]
